5ce1a81c206eec5f3390973aca8cf653287d97e0
nsnwsp <danilapenna.dlp@gmail.com>
2024-03-12 17:15:20 +0100

    defined a responsive layout template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Button, Grid, GridItem, Show, useColorMode } from "@chakra-ui/react";
+
 function App() {
-  return <p>game lib</p>;
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const templateArea = {
+    base: '"nav" "main"',
+    lg: '"nav nav" "aside main"', // wider then 1024
+  };
+  return (
+    <>
+      <Grid templateAreas={templateArea}>
+        <GridItem area="nav" bg="coral">
+          nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import theme from "./components/theme";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
