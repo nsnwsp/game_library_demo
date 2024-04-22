837ab4337b0cc5deab47119a9d673b59013585d6
nsnwsp <danilapenna.dlp@gmail.com>
2024-04-22 14:27:39 +0200

    fixed issues related to invalid data from backend

--- src/components/GameCriticScore.tsx
+++ src/components/GameCriticScore.tsx
@@ -6,6 +6,18 @@ interface Props {
 }
 
 function GameCriticScore({ score }: Props) {
+  if (isNaN(score))
+    return (
+      <Badge
+        colorScheme={"grey"}
+        fontSize="14"
+        paddingX="2"
+        borderRadius="5"
+        variant="solid"
+      >
+        NA
+      </Badge>
+    );
   const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
     <Badge
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from "../assets/no-image-placeholder.svg";
 const getCroppedImageUrl = (url: string): string => {
-  if (!url) return "";
+  if (!url) return noImage;
   return url.replace("/media/", "/media/crop/600/400/");
 };
 
