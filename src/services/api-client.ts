9926fa67513ac4c7f9c4766017e539b7a685c0a6
nsnwsp <danilapenna.dlp@gmail.com>
2024-09-05 19:15:57 +0200

    refreshed and removed API key from Git

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+const apiKey = import.meta.env.VITE_API_KEY;
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "124d2bbf19734cb3b43f031fb5e0e3b5",
+    key: apiKey,
   },
 });
